9bd2c38a71d7a31ae891ee2129f62df4363f014f
prashant <prashantnk2001@gmail.com>
2022-03-07 23:11:08 +0530

    add lazy , eager loading

--- src/com/hibernate/demo/EagerLazyFetching.java
+++ src/com/hibernate/demo/EagerLazyFetching.java
@@ -31,16 +31,17 @@ public class EagerLazyFetching {
 // by session.createQuery
 
 			final String hql = "select i from Instructor i join fetch i.courses where i.id = :theInstructorId";
+
+//			updating using hql
 			final String updateInsdetail = "update Instructor i set i.instructorDetail=:theInstructorDetail where i.id=2";
+			session.createQuery(updateInsdetail)
+					.setParameter("theInstructorDetail", session.get(InstructorDetail.class, 2)).executeUpdate();
 
 			Query<Instructor> q = session.createQuery(hql, Instructor.class);
 
 			q.setParameter("theInstructorId", 2);
 
 			var i = q.getSingleResult();
-
-			session.createQuery(updateInsdetail)
-					.setParameter("theInstructorDetail", session.get(InstructorDetail.class, 2)).executeUpdate();
 
 			System.out.println(i);
 			System.out.println(i.getCourses());
